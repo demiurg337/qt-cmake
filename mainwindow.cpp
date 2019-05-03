48a3532e85ae695b5bb53670d7f75019c2e13403
demiurg337 <dmitro.gedz@gmail.com>
2019-05-03 17:46:14 +0300

    First signals for button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>5
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->sendMsgButton, &QPushButton::clicked, this, &MainWindow::sendMsg);
+}
+
+void MainWindow::sendMsg()
+{
+    qDebug() << "TTTTTTTT";
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
+    void sendMsg();
+
 private:
     Ui::MainWindow *ui;
 };
